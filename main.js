01f7c7ec1b194225c79c6e9e0b3b978f936ec88a
D-Kunrath <dkunrath@gmail.com>
2023-11-23 18:27:45 -0300

    exercícios feitos

---
+++ main.js
@@ -0,0 +1,72 @@
+// ex. 01
+
+console.log("Hello World!");
+
+//  ex. 02
+
+const n1 = 123;
+const n2 = 456;
+console.log(n1 + n2);
+
+// ex. 03
+
+const n3 = 4;
+if (typeof n3 === "number") {
+  console.log("É um número");
+} else {
+  console.log("Não é um número");
+}
+
+// ex. 04
+
+const n4 = "olá!";
+if (typeof n4 === "string") {
+  console.log("É uma string");
+} else {
+  console.log("Não é uma string");
+}
+
+// ex. 05
+
+const n5 = true;
+if (typeof n5 === "boolean") {
+  console.log("É um boolean");
+} else {
+  console.log("Não é um boolean");
+}
+
+// ex. 06
+
+const n6 = 789;
+const n7 = 123;
+console.log(n6 - n7);
+
+// ex. 07
+
+const n8 = 456;
+const n9 = 789;
+console.log(n8 * n9);
+
+// ex. 08
+
+const n10 = 123;
+const n11 = 456;
+console.log(n10 / n11);
+
+// ex. 09
+
+const n12 = 789;
+if (n12 % 2 === 0) {
+  console.log("É um número par");
+} else {
+  console.log("Não é um número par");
+}
+
+// ex. 10
+
+const n13 = 123;
+if (n12 % 2 !== 0) {
+  console.log("É um número ímpar");
+} else {
+  console.log("Não é um número ímpar");
+}
